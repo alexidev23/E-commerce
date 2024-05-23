4a0e1c6bcb52a885ab04d3ee4bd89d47d60d5e0f
AlexisDev <alexisescobar1234@gmail.com>
2024-05-23 08:52:35 -0300

    add icons components

---
+++ src/components/icons/MenuIcon.jsx
@@ -0,0 +1,5 @@
+export function MenuIcon () {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round' className='icon icon-tabler icons-tabler-outline icon-tabler-menu-2'><path d='M0 0h24v24H0z' stroke='none' /><path d='M4 6h16M4 12h16M4 18h16' /></svg>
+  )
+}
