58529fce9e2cb3aed3e71b543375f3ca102f554a
AlexisDev <alexisescobar1234@gmail.com>
2024-06-07 19:18:37 -0300

    add component whatsapp

---
+++ src/components/Whatsapp.jsx
@@ -0,0 +1,7 @@
+export function Whatsapp () {
+  return (
+    <button className='fixed bg-[#09D204] h-20 w-20 bottom-6 right-6 rounded-full flex items-center justify-center'>
+      <img src='/whatsapp.svg' alt='Icono de Whatsapp' width='40px' />
+    </button>
+  )
+}
