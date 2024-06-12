d5067537da5a09e8cc33a3e4dcbbdc45eacf3f6e
AlexisDev <alexisescobar1234@gmail.com>
2024-06-11 21:53:03 -0300

    add icons

---
+++ src/components/icons/ShopCartEmptyIcon.jsx
@@ -0,0 +1,5 @@
+export function ShopCartEmptyIcon () {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round' className='icon icon-tabler icons-tabler-outline icon-tabler-shopping-cart-off'><path d='M0 0h24v24H0z' stroke='none' /><path d='M4 19a2 2 0 1 0 4 0 2 2 0 1 0-4 0M17 17a2 2 0 1 0 2 2' /><path d='M17 17H6V6M9.239 5.231 20 6l-1 7h-2m-4 0H6M3 3l18 18' /></svg>
+  )
+}
---
+++ src/components/icons/TrashIcon.jsx
@@ -0,0 +1,5 @@
+export function TrashIcon () {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round' className='icon icon-tabler icons-tabler-outline icon-tabler-trash'><path d='M0 0h24v24H0z' stroke='none' /><path d='M4 7h16M10 11v6M14 11v6M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2l1-12M9 7V4a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v3' /></svg>
+  )
+}
